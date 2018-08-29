015590dc9ff6fc9330dafd574ca8d594552d93e7
blacksavage <510381706@qq.com>
2018-08-30 00:34:33 +0800

    0.1

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,12 +1,12 @@
 module.exports = (isDev) => {
-    return {
-        // 阻止元素间生成空白内容
-        preserveWhitepace: true,
-        // 将vue文件内的css单独打包到一个文件
-        extractCSS: !isDev,
-        cssModules: {
-            localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
-            camelCase: true
-        }
+  return {
+    // 阻止元素间生成空白内容
+    preserveWhitepace: true,
+    // 将vue文件内的css单独打包到一个文件
+    extractCSS: !isDev,
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      camelCase: true
     }
+  }
 }
