c99f6f44c1655f59243c012e577c5e6932691194
blacksavage <510381706@qq.com>
2018-10-09 00:35:14 +0800

    0.1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 
 const app = new Koa()
 
+const pageRouter = require('./routers/dev-ssr')
+
 const isDev = process.env.NODE_ENV === 'development'
 
 app.use(async (ctx, next) => {
@@ -17,4 +19,13 @@ app.use(async (ctx, next) => {
       ctx.body = 'please try again later'
     }
   }
+})
+
+app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
+
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, HOST, () => {
+  console.log(`server is listening on ${HOST}:${PORT}`)
 })
